c682b822a8b3cd94c7242a1c422ccc318470c66f
Felix kimutai <kmtflx@gmail.com>
2020-09-21 23:34:21 +0300

    main method commit continuation

--- LinkedListMPlayer/src/linkedlistmplayer/LinkedListMPlayer.java
+++ LinkedListMPlayer/src/linkedlistmplayer/LinkedListMPlayer.java
@@ -2,14 +2,10 @@
 package linkedlistmplayer;
 
 import java.util.ArrayList;
-import java.util.List;
 
-/**
- *
- * @author Kimz
- */
+
 public class LinkedListMPlayer {
-
+private static final ArrayList<Album> albums = new ArrayList<>();
     public static void main(String[] args) {
         //album name and the name of the artist
       Album album = new Album("The Colour Of My Love", "Celine");
@@ -23,7 +19,21 @@ public class LinkedListMPlayer {
       album.addSong("Love doesn't ask why", 4.08);
       album.addSong("When I fall in love", 4.20);
         
-
+      albums.add(album); //adding to arraylist 
+      
+      //anther album and songs will be added 
+      album = new Album("The Women in me", "Shania");
+      album.addSong("What made you say that", 2.59);
+      album.addSong("You Lay a Whole Lot of Love on Me", 2.48);
+      album.addSong("Dance With the One That Brought You", 2.24);
+      album.addSong("Still Under the Weather", 3.07);
+      album.addSong("God Ain't Gonna Getcha for That", 2.45);
+      album.addSong("Got a Hold on Me", 2.14);
+      album.addSong("There Goes the Neighborhood", 3.17);
+      album.addSong("Forget Me", 3.21);
+      album.addSong("When he leaves you", 4.21);
+      album.addSong("Crime of the Century", 3.29);
+      
       
     }
     
