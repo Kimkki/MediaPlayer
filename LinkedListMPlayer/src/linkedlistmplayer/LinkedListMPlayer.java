6b211695407d168aa637b423f60cec148219808b
Felix kimutai <kmtflx@gmail.com>
2020-09-21 23:21:02 +0300

    main method commit

---
+++ LinkedListMPlayer/src/linkedlistmplayer/LinkedListMPlayer.java
@@ -0,0 +1,30 @@
+
+package linkedlistmplayer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Kimz
+ */
+public class LinkedListMPlayer {
+
+    public static void main(String[] args) {
+        //album name and the name of the artist
+      Album album = new Album("The Colour Of My Love", "Celine");
+      //names of the songs in the album
+      album.addSong("The power of love", 5.43);
+      album.addSong("Misled", 3.31);
+      album.addSong("Think Twice", 4.48);
+      album.addSong("Only one road", 4.49);
+      album.addSong("Everybody talkin", 4.02);
+      album.addSong("Next Plane out", 4.59);
+      album.addSong("Love doesn't ask why", 4.08);
+      album.addSong("When I fall in love", 4.20);
+        
+
+      
+    }
+    
+}
